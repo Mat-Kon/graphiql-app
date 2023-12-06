d45736fa7dcbbf32bbc3018981758c263d38cf3e
Mat-Kon <mat_kon@mail.ru>
2023-12-06 21:43:46 +0300

    feat: create Footer component

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,33 @@
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="authors">
+        <ul className="authors__list">
+          <li className="authors__item">
+            <a href="#" className="authors__link">
+              Konstantin
+            </a>
+          </li>
+          <li className="authors__item">
+            <a href="#" className="authors__link">
+              Eugenia
+            </a>
+          </li>
+          <li className="authors__item">
+            <a href="#" className="authors__link">
+              Olya
+            </a>
+          </li>
+        </ul>
+      </div>
+
+      <div className="year">
+        <h5>2023</h5>
+      </div>
+
+      <div className=""></div>
+    </footer>
+  );
+};
+
+export default Footer;
