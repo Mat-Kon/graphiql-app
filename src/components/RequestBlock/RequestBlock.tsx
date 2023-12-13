8b37008c38ae894555b110ef3ed7234abdb8a833
Olga <kartoshkaslukom@gmail.com>
2023-12-13 19:39:15 +0100

    feat: add changing endpoints

--- src/components/RequestBlock/RequestBlock.tsx
+++ src/components/RequestBlock/RequestBlock.tsx
@@ -1,14 +1,24 @@
 import EditorWrapper from '../EditorWrapper/EditorWrapper';
 import RequstBtnsContainer from '../RequstBtnsContainer/RequstBtnsContainer';
+import { useState } from 'react';
 import styles from './request.module.css';
 
 const RequestBlock = () => {
+  const [url, setUrl] = useState('');
   return (
     <div className={styles.req__container}>
       <div className={styles.req__header}>
-        <textarea id={styles.req__url} rows={1} placeholder="URL"></textarea>
+        <textarea
+          id={styles.req__url}
+          rows={1}
+          placeholder="URL"
+          value={url}
+          onChange={(e) => {
+            setUrl(e.target.value);
+          }}
+        ></textarea>
       </div>
-      <RequstBtnsContainer />
+      <RequstBtnsContainer url={url} />
       <EditorWrapper />
       <div className={styles.req__heders_variables}>
         <div>
