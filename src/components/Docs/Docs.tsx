271c7af5bc4411b23d0d3d08ead760f255f12c30
iozefavichus <iozefavichus@gmail.com>
2024-01-03 20:42:01 +0200

    fix: add translation for docs button, change color for documentation text, add error text for

--- src/components/Docs/Docs.tsx
+++ src/components/Docs/Docs.tsx
@@ -1,15 +1,17 @@
 import { useState } from 'react';
 import styles from './docs.module.css';
 import DocsMain from './DocsMain';
+import { useLanguageContext } from '../../utils/hooks/useLangContext';
 
 const Docs = () => {
   const [isOpen, setOpen] = useState(false);
+  const { translations, currentLanguage } = useLanguageContext();
 
   return isOpen ? (
     <div className={styles.docs_container_open} data-testid="docs">
       <div className={styles.btn_wrap}>
         <button className={styles.btn} onClick={() => setOpen(!isOpen)}>
-          Close docs
+          {translations[currentLanguage].closeDocsBtn}
         </button>
       </div>
       <DocsMain />
@@ -18,7 +20,7 @@ const Docs = () => {
     <div className={styles.docs_container_close} data-testid="docs">
       <div className={styles.btn_wrap}>
         <button className={styles.btn_open} onClick={() => setOpen(!isOpen)}>
-          Docs
+          {translations[currentLanguage].docsBtn}
         </button>
       </div>
     </div>
